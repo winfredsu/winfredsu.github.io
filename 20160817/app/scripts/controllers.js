7d7802636471fc88fa899c19cc725064385ba266
unknown <>
2016-08-01 10:50:03 +0800

    Site updated: 2016-08-01 10:50:02

--- 20160817/app/scripts/controllers.js
+++
@@ -1,9 +0,0 @@
-//'use strict';
-//
-//angular.module('hrApp');
-//
-//.controller('SomeController', ['$rootScope', function ($rootScope) {
-//    $rootScope.$on('$viewContentLoaded', function () {
-//        homepageResponsive();
-//    });
-//}]);
